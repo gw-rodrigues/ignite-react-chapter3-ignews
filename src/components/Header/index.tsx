2207f61a521aaaf2664850969de13e70aa59127f
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-04-22 10:29:29 +0200

    imporing siginbutton

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
-import styles from './styles.module.scss'
+import styles from './styles.module.scss';
+import { SignInButton } from '../signInButton';
 export function Header(){
     return(
         <header className={styles.headerContainer}>
@@ -8,7 +9,8 @@ export function Header(){
                     <a className={styles.active} href="/">Home</a>
                     <a href="/posts">Posts</a>
                 </nav>
+                <SignInButton />
             </div>
-        </header>
+        </header>   
     )
 }
