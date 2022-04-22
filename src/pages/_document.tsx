62923aa03946e341dcf8a6fd06a74ae2a91df112
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-04-22 08:55:47 +0200

    Add fonts, and elements

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Roboto:wght@700;900&display=swap" rel="stylesheet" />
+                    <link href="favicon.png" rel="shortcut icon" type="image/png"/>
+                </Head>
+                <body>
+                    <Main /><NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
