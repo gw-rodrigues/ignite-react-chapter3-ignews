fe1fe9576a60e5944d7375a077b696dd2e29eea9
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-04-22 10:58:08 +0200

    importing subscribebutton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { SubscribeButton } from '../components/subscribeButton'
 import styles from './home.module.scss'
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
                 <span>👏 Hey, welcome</span>
                 <h1>News about the <span>React</span> world.</h1>
                 <p>Get access to all the publications <br/> <span>for $9.90 month</span></p>
+                <SubscribeButton/>
             </section>
             <img src="/images/avatar.svg" alt="girl coding" />
         </main>
