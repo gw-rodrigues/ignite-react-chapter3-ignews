b130f64fa4cd1cee77d62b99d3b14a24c7587388
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-04-22 10:29:49 +0200

    Add component signinbutton

--- src/components/signInButton/index.tsx
+++ src/components/signInButton/index.tsx
@@ -1,5 +1,20 @@
-export function signInButton(){
-    return(
-        <button type="button">Sign in with Github</button>
-    )
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+import styles from './styles.module.scss'
+export function SignInButton() {
+    const isUserLoggedIn = false;
+    
+    return isUserLoggedIn ?       
+        (
+            <button type="button" className={styles.signInButton}>
+                <FaGithub color='#eba417' />Sign in with Github
+            </button>
+        )
+        :
+        (
+            <button type="button" className={styles.signInButton}>
+                <FaGithub color='#eba417' />
+                Gleydson Rodrigues <FiX color="#737380" className={styles.closeIcon} />
+            </button>  
+        )
 }
