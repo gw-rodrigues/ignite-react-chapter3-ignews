9e570a865d1179da7631d45682182975997ce605
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-04-22 08:56:36 +0200

    creating heder component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import styles from './styles.module.scss'
+export default function Header(){
+    return(
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="ig.news" />
+                <nav>
+                    <a className={styles.active} href="/">Home</a>
+                    <a href="/posts">Posts</a>
+                </nav>
+            </div>
+        </header>
+    )
+}
