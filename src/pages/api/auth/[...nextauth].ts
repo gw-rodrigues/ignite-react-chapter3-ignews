11c31d34d635265909795799162e159f0ce00880
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-04-23 19:59:37 +0200

    add fauna query

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { query as q } from "faunadb"
+import { fauna } from "../../../services/fauna"
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -7,12 +9,45 @@ export default NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      authorization:{
-          params:{
-              scope:'read:user' //+1 params no scope 'read:user, read:repo, etc...'
-          }
+      authorization: {
+        params: {
+          scope: 'read:user' //+1 params no scope 'read:user, read:repo, etc...'
+        }
       }
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ), //end condition
+            //if true
+            q.Create(
+              q.Collection('users'), { data: { email } }
+            ),
+            //if false
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )  
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
 })
