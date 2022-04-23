319c3a69a80050d739bccc97aa7dc8befdec9ecc
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-04-23 19:31:39 +0200

    add signout

--- src/components/signInButton/index.tsx
+++ src/components/signInButton/index.tsx
@@ -1,4 +1,4 @@
-import { signIn, useSession } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss'
@@ -7,8 +7,8 @@ export function SignInButton() {
     
     return session ?       
         (
-            <button type="button" className={styles.signInButton}>
-                <FaGithub color='#eba417' />
+            <button type="button" className={styles.signInButton} onClick={()=>signOut()}>
+                <FaGithub color='#04d361' />
                 {session.user.name} <FiX color="#737380" className={styles.closeIcon} />
             </button> 
         )
