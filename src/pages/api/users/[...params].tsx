79d17e2b827ce619c8b270b731e42488571f314a
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-04-23 18:26:19 +0200

    Renaming, getting more params

--- src/pages/api/users/[...params].tsx
+++ src/pages/api/users/[...params].tsx
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 export default (request:NextApiRequest, response:NextApiResponse)=>{
-    console.log(request.query.id)
+    console.log(request.query)
     const users = [
         {id:1, name:'Diego'},
         {id:2, name:'Rodrigo'},
