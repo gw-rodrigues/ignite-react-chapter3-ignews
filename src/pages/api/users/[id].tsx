1a09c72b5d61cae279686095435a6bd66467b863
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-04-23 18:22:14 +0200

    Add request by id

---
+++ src/pages/api/users/[id].tsx
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from "next";
+export default (request:NextApiRequest, response:NextApiResponse)=>{
+    console.log(request)
+    const users = [
+        {id:1, name:'Diego'},
+        {id:2, name:'Rodrigo'},
+        {id:3, name:'Alex'},
+    ]
+    return response.json(users)
+}
