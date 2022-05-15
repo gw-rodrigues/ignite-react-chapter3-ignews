d44b1b53c668ceb8c7914451ce8a3a3515eb274c
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-05-15 17:50:18 +0200

    checking and logging user when subscribe clicked

--- src/components/subscribeButton/index.tsx
+++ src/components/subscribeButton/index.tsx
@@ -1,13 +1,25 @@
-import styles from './styles.module.scss';
+import { signIn, useSession } from "next-auth/react";
+import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
-    priceId: string;
+  priceId: string;
 }
 
-export function SubscribeButton({ priceId }:SubscribeButtonProps){
-    return(
-        <button type='button' className={styles.subscribeButton}>
-            Subscribe now
-        </button>
-    )
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+  }
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
+  );
 }
