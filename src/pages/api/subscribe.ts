d34524500346d97e4ca441048518524c3ca156f2
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-05-16 16:40:04 +0200

    avoinding duplicates in stripe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,7 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { stripe } from "../../services/stripe";
+import { query as q } from "faunadb";
 import { getSession } from "next-auth/react";
-import { strictEqual } from "assert";
+import { fauna } from "../../services/fauna";
+import { stripe } from "../../services/stripe";
+
+type User = {
+  ref: { id: string };
+  data: { stripe_customer_id: string };
+};
 
 export default async function Subscribe(
   req: NextApiRequest,
@@ -9,12 +15,25 @@ export default async function Subscribe(
 ) {
   if (req.method === "POST") {
     const session = await getSession({ req });
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user.email,
-      //metadata
-    });
+    const user = await fauna.query<User>(
+      q.Get(q.Match(q.Index("user_by_email"), q.Casefold(session.user.email)))
+    );
+    let customerId = user.data.stripe_customer_id;
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+        //metadata
+      });
+
+      await fauna.query(
+        q.Update(q.Ref(q.Collection("users"), user.ref.id), {
+          data: { stripe_customer_id: stripeCustomer.id },
+        })
+      );
+      customerId = stripeCustomer.id;
+    }
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id, //dos cookies apos esta loggado
+      customer: customerId, //dos cookies apos esta loggado
       payment_method_types: ["card"], //outros
       billing_address_collection: "required", //ou Auto config no stripe
       line_items: [
@@ -29,8 +48,8 @@ export default async function Subscribe(
       cancel_url: process.env.STRIPE_CANCEL_URL,
     });
     return res.status(200).json({ sessionId: stripeCheckoutSession.id }); //com id vamos/pesquisamos depois user/converte url
-  }else{
-      res.setHeader('Allow','POST')
-      res.status(405).end('Method not allowed')
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method not allowed");
   }
 }
