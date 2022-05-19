527ebc33604369cf24148103b28e8e6b02ce8236
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-05-19 21:17:21 +0200

    updating prismic client

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,4 +1,41 @@
-import Prismic from "@prismicio/client";
+import * as prismic from "@prismicio/client";
+import { enableAutoPreviews } from "@prismicio/next";
+import sm from "../../sm.json";
+
+// Update the Link Resolver to match your project's route structure
+export function linkResolver(doc) {
+  switch (doc.type) {
+    case 'homepage':
+      return '/'
+    case 'posts':
+      return `/${doc.uid}`
+    default:
+      return null
+  }
+}
+
+// This factory function allows smooth preview setup
+export const repositoryName = prismic.getRepositoryName(
+  process.env.PRISMIC_ENDPOINT
+);
+
+// This factory function allows smooth preview setup
+export function getPrismicClient(config?:any) {
+  const client = prismic.createClient(process.env.PRISMIC_ENDPOINT, {
+    ...config,
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+  });
+
+  enableAutoPreviews({
+    client,
+    previewData: config?.previewData,
+    req: config?.req,
+  })
+
+  return client;
+}
+
+/* import Prismic from "@prismicio/client";
 
 export function getPrismicClient(req = {}) {
   const prismic = Prismic.createClient(process.env.PRISMIC_ENDPOINT, {
@@ -6,4 +43,4 @@ export function getPrismicClient(req = {}) {
     accessToken:process.env.PRISMIC_ACCESS_TOKEN
   });
   return prismic
-}
+} */
