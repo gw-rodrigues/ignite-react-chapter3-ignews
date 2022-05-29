02671930233d842236cff9cf6525115f9d8575f5
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-05-29 17:03:10 +0200

    Using next/link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,22 @@
-import styles from './styles.module.scss';
-import { SignInButton } from '../signInButton';
-export function Header(){
-    return(
-        <header className={styles.headerContainer}>
-            <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="ig.news" />
-                <nav>
-                    <a className={styles.active} href="/">Home</a>
-                    <a href="/posts">Posts</a>
-                </nav>
-                <SignInButton />
-            </div>
-        </header>   
-    )
+import styles from "./styles.module.scss";
+import { SignInButton } from "../signInButton";
+import Link from "next/link";
+import { ActiveLink } from "../ActiveLink";
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+          <Link href="/posts">
+            <a>Posts</a>
+          </Link>
+        </nav>
+        <SignInButton />
+      </div>
+    </header>
+  );
 }
