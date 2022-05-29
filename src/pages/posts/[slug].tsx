02ac7b013a08deb2c9929f6000c12a2985f0aa69
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-05-29 18:31:31 +0200

    add user validation

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -39,6 +39,16 @@ export const getServerSideProps: GetServerSideProps = async ({
 }) => {
   const session = await getSession({ req });
   const { slug } = params;
+
+  if(!session.activeSubscription){
+    return {
+        redirect: {
+            destination: '/',
+            permanent: false
+        }
+    }
+  }
+
   const prismic = getPrismicClient(req);
   const response = await prismic.getByUID("posts", String(slug), {});
   const post = {
