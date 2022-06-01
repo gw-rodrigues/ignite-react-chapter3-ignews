1d3e1fd58d9616c1accf9dc5968de11fa01fb3b9
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-06-01 15:21:46 +0200

    check session is null

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -40,7 +40,7 @@ export const getServerSideProps: GetServerSideProps = async ({
   const session = await getSession({ req });
   const { slug } = params;
 
-  if(!session.activeSubscription){
+  if(!session?.activeSubscription){
     return {
         redirect: {
             destination: '/',
