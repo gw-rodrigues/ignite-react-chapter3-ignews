7fbd2f27c2d50d4851f87ac7f41a9a68a4de6d6b
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-14 10:42:20 +0200

    ADD configure Jest for tests

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * Temos que configurar o babel para nao dar erro.
+ * 
+ * vamos usar "next/babel" podia usar o "babel-preset-env","babel-preset-react","babel-preset-typescript"
+ * - mas como o next/babel ja tem tudo incluído e mais fácil
+ * 
+ */
+module.exports = {
+    presets:['next/babel']
+}
